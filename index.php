66e0d2c890ebab5d819bb6d9ca24a87a06fd6fea
rokeyzki <rokeyzki@gmail.com>
2015-12-13 20:21:10 +0800

    delete

--- index.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * @Author: Charles Lim
- * @Date:   2015-12-13 14:27:53
- * @Last Modified by:   Charles Lim
- * @Last Modified time: 2015-12-13 14:28:05
- */
-
-phpinfo();
--- start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# @Author: Charles Lim
-# @Date:   2015-12-13 13:19:04
-# @Last Modified by:   Charles Lim
-# @Last Modified time: 2015-12-13 14:25:14
-
-source /etc/apache2/envvars
-tail -F /var/log/apache2/* &
-exec apache2 -D FOREGROUND
