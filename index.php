b21930d7788519387036e3097858c436f5aab378
rokeyzki <rokeyzki@gmail.com>
2015-12-13 14:29:26 +0800

    docker

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * @Author: Charles Lim
+ * @Date:   2015-12-13 14:27:53
+ * @Last Modified by:   Charles Lim
+ * @Last Modified time: 2015-12-13 14:28:05
+ */
+
+phpinfo();
--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 # @Author: Charles Lim
 # @Date:   2015-12-13 13:19:04
 # @Last Modified by:   Charles Lim
-# @Last Modified time: 2015-12-13 13:19:17
+# @Last Modified time: 2015-12-13 14:25:14
 
 source /etc/apache2/envvars
 tail -F /var/log/apache2/* &
