fac9bf673c2117e36bdc6defd9cd6a9594083cc2
鸦片 <rokeyzki@foxmail.com>
2015-12-22 11:30:12 +0800

    mongo

--- 4.Serve/Koa/mongo.js
+++ 4.Serve/Koa/mongo.js
@@ -1,30 +1,105 @@
-/*
-* @Author: Charles Lim
-* @email:  hi@oulve.com
-* @Date:   2015-12-15 16:52:25
-* @Last Modified by:   Charles Lim
-* @Last Modified time: 2015-12-17 15:23:33
-*/
-
 var app      = require('koa')(), // 框架内核
-    router   = require('koa-router')(), // 路由中间件
-    gzip     = require('koa-gzip'), // gzip压缩（待确认是否有效执行）
+    router   = require('koa-router')() // 路由中间件
     mongoose = require('mongoose');
 
-mongoose.connect('mongodb://127.0.0.1:27017/foo');
+var db = mongoose.connection;    
 
-var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
+db.on('error',console.error);
+db.once('open',function(){
+  console.log('ok');
+  //在这里创建你的模式和模型
+  // Schema 模式
+  var BookSchema = new mongoose.Schema({
+    name: String,
+    age: Number,
+    time: Date
+  });
+  // Model 模型
+  mongoose.model('Book', BookSchema);
+  
+  // db save
+  var Book = mongoose.model('Book');
+  
+  var book = new Book({
+    name: "rokey",
+    age: "28",
+    time: new Date()
+  })
+  
+  book.save(function(err){
+    console.log('save status:', err ? 'failed' : 'success');
+  });
+  
+  // db find
+  Book.find({}, function(err, docs){
+    if(err){
+      console.log('err:', err);
+      return;
+    }else{
+      console.log('result:', docs);
+    }
+  });
+  
+  /*Book.findOne({_id:"5672d4a4b969af4410aa33fd"}, function(err, docs){
+    if(err){
+      console.log('err:', err);
+      return;
+    }else{
+      docs.name = "Jame";
+      docs.save();
+      console.log('result:', docs);
+    }
+  });
+  // db remove
+  Book.findOne({_id:"5672d5bb46b1a0eb11291d90"}, function(err, docs){
+    if(err){
+      console.log('err:', err);
+      return;
+    }
+    if(docs){
+      docs.remove(function(err){
+        console.log('remove status:', err ? 'failed' : 'success');
+      });
+    }
+  });*/
+  // db update
+  var cond = {
+    $or: [
+      {_id: '5672d66fa5e9edec12067a1d'},
+      {name: 'Jame'}
+    ]
+  }
+  Book.find(cond, function(err, docs){
+    if(err){
+      console.log('err:', err);
+      return;
+    }
+    if(docs){
+      for(var i in docs){
+        docs[i].age = 29;
+        docs[i].save();
+      }
+    }
+  });
+});   
 
-router.get('/db',
+// mongoose.connect('mongodb://127.0.0.1:10000/foo');
+mongoose.connect('mongodb://127.0.0.1/foo');
+
+router.get('/',
   function *(next) {
   	console.log('d1');
-    // console.log(this);
   	this.body = JSON.stringify('hello mongo');
+  	yield next;
+  },
+  
+  function *(next) {
+    console.log('d2');
+    yield next;
   }
 );
 
-app.use(gzip());
+// app.use(cors());
 app.use(router.routes());
 
 app.listen(3000);
