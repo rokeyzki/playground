8600439d3cd43f3fb7f50b1bd82f9847ed95b8ad
rokeyzki <rokeyzki@gmail.com>
2016-01-07 00:30:30 +0800

    pintu

---
+++ 1.Native/HTML/pintuer_16.01.07/asset/base/js/oulve-nitialize.js
@@ -0,0 +1,6 @@
+/* SignOulve CSS 防盗用机关 初始化文件，未加密稿 */
+if(document.domain === 'coding.oulve.com'){
+    document.write('<link rel="stylesheet" href="../asset/base/css/oulve.min.css"><link rel="stylesheet" href="../asset/self/css/style.css">');
+}else{
+    window.location.href="http://oulve.com";
+}
