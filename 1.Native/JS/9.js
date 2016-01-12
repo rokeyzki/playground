72c07779de7957b5337a968b7264bd8c853f4e12
rokeyzki <rokeyzki@gmail.com>
2016-01-12 13:26:37 +0800

    es6

--- 1.Native/JS/9.js
+++
@@ -1,9 +0,0 @@
-/*
-* @Author: Charles Lim
-* @Email:  hi@oulve.com
-* @Date:   2015-12-11 22:29:52
-* @Last Modified by:   Charles Lim
-* @Last Modified time: 2015-12-11 22:29:52
-*/
-
-'use strict';
