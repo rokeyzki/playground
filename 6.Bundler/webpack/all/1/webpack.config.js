cba55d639398f93b5162163444c12d1ac32fc4c8
鸦片 <rokeyzki@foxmail.com>
2016-01-17 14:47:38 +0800

    image

--- 6.Bundler/webpack/all/1/webpack.config.js
+++ 6.Bundler/webpack/all/1/webpack.config.js
@@ -1,16 +1,18 @@
 var ExtractTextPlugin = require("extract-text-webpack-plugin"); // 该插件使CSS单独打包成一个文件：sudo npm install extract-text-webpack-plugin
 
 module.exports = {
-  entry: "./entry.js",
+  entry: {
+    mainpage: './index.js',
+  },
   output: {
-    path: "./assets",
-    filename: "app.js"
+    path: './assets',
+    filename: '[name].min.js'
   },
   module: {
     loaders: [
       { test: /\.jsx$/,
         exclude: /(node_modules|bower_components)/,
-        loader: 'babel', // 'babel-loader' is also a legal name to reference
+        loader: 'babel-loader',
         query: {
           presets: ['react', 'es2015'] // es6转es5 依赖 babel-preset-es2015，而jsx转es5 依赖 babel-preset-react，详见 https://www.npmjs.com/package/babel-loader
         }
@@ -26,15 +28,13 @@ module.exports = {
       },{
         test: /\.(jpe?g|png|gif|svg)$/i,
         loaders: [
-          'image?{bypassOnDebug: true, progressive:true, \
-                optimizationLevel: 3, pngquant:{quality: "65-80"}}',
-          'url?limit=10000&name=img/[hash:8].[name].[ext]'
+          'url-loader?limit=10000&name=img/[hash:8].[ext]' // sudo npm install url-loader
         ]
       }
     ]
   },
   // Use the plugin to specify the resulting filename (and add needed behavior to the compiler)
   plugins: [
-    new ExtractTextPlugin("style.css")
+    new ExtractTextPlugin('[name].min.css')
   ]
 };
